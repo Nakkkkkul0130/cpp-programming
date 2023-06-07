8025f0ac8476a3fb860de478cceb72c0c254d8e2
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-06-08 00:09:39 +0530

    changes

--- main.cpp
+++ main.cpp
@@ -4,5 +4,11 @@ int main()
 {
     cout<<"hello world"<<endl; // <<endl is for new line character
     cout<<"nakul bhar";
+
+
+    int a;  //variable declare
+    a = 23;
+    cout<<"number is "<<a; 
+    // number is 23
     return 0;
 }
