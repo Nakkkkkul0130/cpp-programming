e5eb844301f97b0f1d82cd986ced0b605d3cda0d
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-06-08 09:30:01 +0530

    change

--- main.cpp
+++ main.cpp
@@ -11,7 +11,15 @@ int main()
     // number is 23
 
     float const score = 34.23; //const is used as for value fix,does not change value of score
-    score = 45.6;
-    cout<<"the score is "<<score;
+    //score = 45.6;
+    cout<<"the score is "<<score<<endl;
+
+    int x, y;
+    cout<<"enter first number "<<endl;
+    cin>>x;  //taking input from user
+
+    cout<<"enter second number "<<endl;
+    cin>>y;
+
     return 0;
 }
