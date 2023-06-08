747705b7c4288b1cf0ba57e94a6de8329ad29864
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-06-08 09:21:01 +0530

    changes

--- main.cpp
+++ main.cpp
@@ -3,12 +3,15 @@ using namespace std;
 int main()
 {
     cout<<"hello world"<<endl; // <<endl is for new line character
-    cout<<"nakul bhar";
-
+    cout<<"nakul bhar"<<endl;
 
     int a;  //variable declare
     a = 23;
-    cout<<"number is "<<a; 
+    cout<<"number is "<<a<<endl; 
     // number is 23
+
+    float const score = 34.23; //const is used as for value fix,does not change value of score
+    score = 45.6;
+    cout<<"the score is "<<score;
     return 0;
 }
