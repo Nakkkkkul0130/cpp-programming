5e3aa2dbd6a908deea66c9af683251b30608ed8e
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-06-10 19:07:39 +0530

    basics

--- patterns2.cpp
+++ patterns2.cpp
@@ -11,14 +11,14 @@ int main()
       1
     */
     // code for pattern 1
-    // for (int i = 1; i <= 5; i++)
-    // {
-    //     for (int j = 1; j <= 6 - i; j++)
-    //     {
-    //         cout << j << " ";
-    //     }
-    //     cout << endl;
-    // }
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 6 - i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
 
     // pattern 2
     /*
@@ -30,40 +30,63 @@ int main()
     */
     // code for pattern 2
 
-    // for (int i = 1; i <= 5; i++)
-    // {
-    //     for (int j = 1; j <= i; j++)
-    //     {
-    //         if ((i + j) % 2 == 0)
-    //         {
-    //             cout << "1 ";
-    //         }
-    //         else
-    //         {
-    //             cout << "0 ";
-    //         }
-    //     }
-    //     cout << endl;
-    // }
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if ((i + j) % 2 == 0)
+            {
+                cout << "1 ";
+            }
+            else
+            {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
 
     // pattern 3
     /*
+            * * * * *
           * * * * *
         * * * * *
       * * * * *
     * * * * *
-  * * * * *
      */
     // code for pattern 3
     for (int i = 1; i <= 5; i++)
     {
-        for (int j = 1; j <= 5-i; j++)
+        for (int j = 1; j <= 5 - i; j++)
         {
-            cout<<"  ";
+            cout << "  ";
         }
-        for (int j=1;j<=5;j++){
-            cout<<"* ";
+        for (int j = 1; j <= 5; j++)
+        {
+            cout << "* ";
         }
-        cout<<endl;
+        cout << endl;
+    }
+
+    // pattern 4
+    /*
+              1
+            1   2
+          1   2   3
+        1   2   3   4
+      1   2   3   4   5
+    */
+    // code for pattern 4
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 5 - i; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
     }
 }
