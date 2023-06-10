6666ec2b46e3d53126e83f6f20ed3d3a03f76e14
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-06-10 18:47:39 +0530

    more patterns

--- patterns2.cpp
+++ patterns2.cpp
@@ -11,38 +11,59 @@ int main()
       1
     */
     // code for pattern 1
-    for (int i = 1; i <= 5; i++)
-    {
-        for (int j = 1; j <= 6 - i; j++)
-        {
-            cout << j << " ";
-        }
-        cout << endl;
-    }
+    // for (int i = 1; i <= 5; i++)
+    // {
+    //     for (int j = 1; j <= 6 - i; j++)
+    //     {
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
 
     // pattern 2
     /*
         1
         0 1
-        1 0 1
+        1 0 1                     // use index number to solve this
         0 1 0 1
         1 0 1 0 1
     */
     // code for pattern 2
 
+    // for (int i = 1; i <= 5; i++)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         if ((i + j) % 2 == 0)
+    //         {
+    //             cout << "1 ";
+    //         }
+    //         else
+    //         {
+    //             cout << "0 ";
+    //         }
+    //     }
+    //     cout << endl;
+    // }
+
+    // pattern 3
+    /*
+          * * * * *
+        * * * * *
+      * * * * *
+    * * * * *
+  * * * * *
+     */
+    // code for pattern 3
     for (int i = 1; i <= 5; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 1; j <= 5-i; j++)
         {
-            if ((i + j) % 2 == 0)
-            {
-                cout << "1 ";
-            }
-            else
-            {
-                cout << "0 ";
-            }
+            cout<<"  ";
         }
-        cout << endl;
+        for (int j=1;j<=5;j++){
+            cout<<"* ";
+        }
+        cout<<endl;
     }
 }
