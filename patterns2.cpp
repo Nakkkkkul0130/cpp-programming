d7a8f959fd0eea973f038dcab8916d983a64a996
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-06-10 14:05:03 +0530

    basic patterns

---
+++ patterns2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // pattern 1
+    /*
+      1 2 3 4 5
+      1 2 3 4
+      1 2 3
+      1 2
+      1
+    */
+    // code for pattern 1
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= 6 - i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+
+    // pattern 2
+    /*
+        1
+        0 1
+        1 0 1
+        0 1 0 1
+        1 0 1 0 1
+    */
+    // code for pattern 2
+
+    for (int i = 1; i <= 5; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            if ((i + j) % 2 == 0)
+            {
+                cout << "1 ";
+            }
+            else
+            {
+                cout << "0 ";
+            }
+        }
+        cout << endl;
+    }
+}
