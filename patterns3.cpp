10de5a0190285490c1023d11ed8911bb6d1f0d7f
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-06-12 23:29:30 +0530

    characters patterns

--- patterns3.cpp
+++ patterns3.cpp
@@ -10,16 +10,16 @@ int main()
         d d d d
         e e e e e
     */
-   char ch = 'a';
-   for (int i=1;i<=5;i++)
-   {
-        for (int j=1;j<=i;j++)
-        {
-            cout<<ch<<" ";
-        }
-        ch++;
-        cout<<endl;
-   }
+//    char ch = 'a';
+//    for (int i=1;i<=5;i++)
+//    {
+//         for (int j=1;j<=i;j++)
+//         {
+//             cout<<ch<<" ";
+//         }
+//         ch++;
+//         cout<<endl;
+//    }
 
     // pattern 2
     /*
@@ -29,16 +29,15 @@ int main()
         a b c d
         a b c d e
     */ 
-
-   for (int i=1;i<=5;i++)
-   {
-        char ch = 'a';
-        for (int j=1;j<=i;j++)
-        {
-            cout<<ch++<<" ";
-        }
-        cout<<endl;
-   }
+    // for (int i=1;i<=5;i++)
+    // {
+    //     char ch = 'a';
+    //     for (int j=1;j<=i;j++)
+    //     {
+    //         cout<<ch++<<" ";
+    //     }
+    //     cout<<endl;
+    // }
 
    // pattern 3
    /*
@@ -49,13 +48,34 @@ int main()
         k l m n o
    */
   // code for pattern 3
-    char ch = 'a';
-    for (int i=1;i<=5;i++)
+    // char ch = 'a';
+    // for (int i=1;i<=5;i++)
+    // {
+    //     for (int j=1;j<=i;j++)
+    //     {
+    //         cout<<ch++<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // pattern 4
+    /*
+        A A A A A
+        B B B B
+        C C C
+        D D
+        E
+    */
+   // code for pattern 4
+    char ch = 'A';
+    for (int i=5;i>=1;i--)
     {
         for (int j=1;j<=i;j++)
         {
-            cout<<ch++<<" ";
+            cout<<ch<<" ";
         }
+        ch++;
         cout<<endl;
+
     }
 }
