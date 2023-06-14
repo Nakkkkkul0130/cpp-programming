8691dcd071b4478cb5b31d4b2253c31dd94c8da0
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-06-14 20:46:11 +0530

    questions practice in array

--- array2.cpp
+++ array2.cpp
@@ -1,7 +1,10 @@
-# include <iostream>
+#include <iostream>
+#include <math.h>
 using namespace std;
 int main()
 {
+    //---------sum of array elements----------
+
     int n;
     cout<<"enter the value of n: ";
     cin>>n;
@@ -17,4 +20,28 @@ int main()
         sum += arr[i];
     }
     cout<<"sum is: "<<sum<<endl;
+
+    //--------finding largest elements in array for number greater than 0-------
+
+    int x;
+    cout << "enter the value of x: ";
+    cin >> x;
+    int arr[x];
+    for (int i = 0; i < x; i++)
+    {
+        cout << "enter the " << i << " th value: ";
+        cin >> arr[i];
+    }
+
+    int largest = 0;
+    for (int i = 0; i < x; i++)
+    {
+        if (arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+    cout << "largest number is: " << largest << endl;
+
+    return 0;
 }
