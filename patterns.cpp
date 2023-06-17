3b157cbce75f272ea5a3726bd02742d2bf9ed1d5
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-06-17 19:19:41 +0530

    patterns

--- patterns.cpp
+++ patterns.cpp
@@ -324,4 +324,32 @@ int main()
         }
         cout<<endl;
    }
+   // pattern 14
+   /*
+        *                *
+        *  *           * *
+        *    *       *   *
+        *      *   *     *
+        *        *       * 
+        *                *
+        *                *
+        *                * 
+        *                * 
+   */
+        // code for pattern 14
+    for (int i=1;i<=9;i++)
+    {
+        for (int j=1;j<=9;j++)
+        {
+            if (j==1 || j==9 || (j==i && i<=5) || (j==10-i && i<=5))
+            {
+                cout<<"* ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+    }
 }
