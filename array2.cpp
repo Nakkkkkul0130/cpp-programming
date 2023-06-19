97a8e22ae34579ac223f9cf28d0e3e513e697aed
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-06-19 23:13:41 +0530

    questions

--- array2.cpp
+++ array2.cpp
@@ -5,95 +5,95 @@ int main()
 {
     //---------sum of array elements----------
 
-    // int n;
-    // cout<<"enter the value of n: ";
-    // cin>>n;
-    // int arr[n];
-    // for (int i=0;i<n;i++)
-    // {
-    //    cout<<"enter the value of "<< i <<"th student: ";
-    //    cin>>arr[i];
-    // }
-    // int sum = 0;
-    // for (int i=0;i<n;i++)
-    // {
-    //     sum += arr[i];
-    // }
-    // cout<<"sum is: "<<sum<<endl;
+    int n;
+    cout<<"enter the value of n: ";
+    cin>>n;
+    int arr[n];
+    for (int i=0;i<n;i++)
+    {
+       cout<<"enter the value of "<< i <<"th student: ";
+       cin>>arr[i];
+    }
+    int sum = 0;
+    for (int i=0;i<n;i++)
+    {
+        sum += arr[i];
+    }
+    cout<<"sum is: "<<sum<<endl;
 
     //--------finding largest elements in array-------
 
-    // int x;
-    // cout << "enter the value of x: ";
-    // cin >> x;
-    // int arr[x];
-    // for (int i = 0; i < x; i++)
-    // {
-    //     cout << "enter the " << i << " th value: ";
-    //     cin >> arr[i];
-    // }
+    int x;
+    cout << "enter the value of x: ";
+    cin >> x;
+    int arr[x];
+    for (int i = 0; i < x; i++)
+    {
+        cout << "enter the " << i << " th value: ";
+        cin >> arr[i];
+    }
 
-    // int largest = INT16_MIN;
-    // for (int i = 0; i < x; i++)
-    // {
-    //     if (arr[i] > largest)
-    //     {
-    //         largest = arr[i];
-    //     }
-    // }
-    // cout << "largest number is: " << largest << endl;
+    int largest = INT16_MIN;
+    for (int i = 0; i < x; i++)
+    {
+        if (arr[i] > largest)
+        {
+            largest = arr[i];
+        }
+    }
+    cout << "largest number is: " << largest << endl;
 
     //--------finding smallest number in array elements---------
-    // int x;
-    // cout << "enter the value of x: ";
-    // cin >> x;
-    // int arr[x];
-    // for (int i = 0; i < x; i++)
-    // {
-    //     cout << "enter the " << i << " th value: ";
-    //     cin >> arr[i];
-    // }
-    // int smallest = INT16_MAX;
-    // for (int i=0;i<x;i++)
-    // {
-    //     if (arr[i] < smallest)
-    //     {
-    //         smallest = arr[i];
-    //     }
-    // }
-    // cout<<"smallest number is: "<<smallest<<endl;
+    int x;
+    cout << "enter the value of x: ";
+    cin >> x;
+    int arr[x];
+    for (int i = 0; i < x; i++)
+    {
+        cout << "enter the " << i << " th value: ";
+        cin >> arr[i];
+    }
+    int smallest = INT16_MAX;
+    for (int i=0;i<x;i++)
+    {
+        if (arr[i] < smallest)
+        {
+            smallest = arr[i];
+        }
+    }
+    cout<<"smallest number is: "<<smallest<<endl;
 
 
-    // return 0;
+    return 0;
 
     //----------swapping of two numbers--------
-    // int a, b;
-    // cout<<"enter value of a: ";
-    // cin>>a;
-    // cout<<"enter the value of b: ";
-    // cin>>b;
-    // int temp = a;
-    // a = b;
-    // b = temp;
-    // cout<<"now value of a is: "<<a<<endl;
-    // cout<<"now value of b is: "<<b;
+    int a, b;
+    cout<<"enter value of a: ";
+    cin>>a;
+    cout<<"enter the value of b: ";
+    cin>>b;
+    int temp = a;
+    a = b;
+    b = temp;
+    cout<<"now value of a is: "<<a<<endl;
+    cout<<"now value of b is: "<<b;
 
-    // return 0;
+    return 0;
 
-    //----------swapping of 2 numbers by second method-----------
+    //----------swapping of 2 numbers by using function SWAP-----------
 
-    // int a, b;
-    // cout<<"before swapping: "<<endl;
-    // cout<<"a = ";
-    // cin>>a;
-    // cout<<"b = ";
-    // cin>>b;
-    // swap(a,b);
-    // cout<<"after swapping: "<<endl;
-    // cout<<"a = "<<a<<endl;
-    // cout<<"b = "<<b;
+    int a, b;
+    cout<<"before swapping: "<<endl;
+    cout<<"a = ";
+    cin>>a;
+    cout<<"b = ";
+    cin>>b;
+    swap(a,b);
+    cout<<"after swapping: "<<endl;
+    cout<<"a = "<<a<<endl;
+    cout<<"b = "<<b;
 
-    //----------reverse of an array-------------
+    //----------array in reverse order-------------
     int n;
     cout<<"enter number of elements: ";
     cin>>n;
@@ -107,5 +107,32 @@ int main()
     {
         cout<<arr[i]<<endl;;
     }
+
+    //-------reverse of array-----------
+    int n;
+    cout<<"enter number of elements in array: ";
+    cin>>n;
+    int arr[n];
+    for (int i=0;i<n;i++)
+    {
+        cout<<"value at "<<i<<"th position is: ";
+        cin>>arr[i];
+    }
+    cout<<endl;
+
+    int start = 0;
+    int end = n-1;
+    while (start<=end)
+    {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+    
+    for (int i=0;i<n;i++)
+    {
+        cout<<"now value at "<<i<<"th position is: "<<arr[i]<<endl;
+    }
+    
 
 }
