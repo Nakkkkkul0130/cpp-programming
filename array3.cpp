2c6bbc46216dc63e02f02e3accf2cda8a6c63d81
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-06-20 17:35:05 +0530

    introduction

--- array3.cpp
+++
@@ -1,7 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    int a[100000000];  // max size array
-    return 0;
-}
---
+++ characterarray.cpp
@@ -0,0 +1,15 @@
+# include <iostream>
+using namespace std;
+int main()
+{
+    // strings - 1D character array
+   // int - 4bites per
+   // char - 1byte per
+
+   char arr[10];
+   cout<<"enter value of arr: ";
+   cin>>arr;
+   // input is coding then from 0-5 coding is print and remaining space have null character "/0" is printed
+   cout<<arr;
+   return 0;
+}
