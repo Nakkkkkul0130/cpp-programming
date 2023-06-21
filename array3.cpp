0c514935a31232626fc35bc6ab8388525ef85ee8
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-06-21 17:28:32 +0530

    character array

--- array3.cpp
+++ array3.cpp
@@ -1,28 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int length(char input[])
-{
-    int count = 0;
-    for (int i = 0; input[i] != '\0'; i++)
-    {
-        count++;
-    }
-    return count;
+// int length(char input[])
+// {
+//     int count = 0;
+//     for (int i = 0; input[i] != '\0'; i++)
+//     {
+//         count++;
+//     }
+//     return count;
 
-}
+// }
 int main()
 {
     // strings - 1D character array
     // int - 4bites per
     // char - 1byte per
 
-    char name[10];
-    cout << "enter your name: ";
-    cin >> name;
-    // input is coding then from 0-5 coding is print and remaining space have null character "/0" is printed
-    cout<<"your name is: "<<name<<endl;
-    cout << length(name);  // length of name
+    // char name[10];
+    // cout << "enter your name: ";
+    // cin >> name;
+    // // input is coding then from 0-5 coding is print and remaining space have null character "/0" is printed
+    // cout<<"your name is: "<<name<<endl;
+    // cout << length(name);  // length of name
+
+    //----------cin.getline function------------
+    char input[50];
+    cout<<"enter the input: ";
+    cin.getline(input,50);   // this function takes 3 paramaters, 3rd one is optional
+    cout<<input;
 
     return 0;
 }
