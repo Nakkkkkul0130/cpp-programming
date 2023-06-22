971137eebd572c5d5a88e6e30cd5aecc9b6b7a6d
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-06-22 21:04:56 +0530

    basic to advance patterns

--- patterns.cpp
+++ patterns.cpp
@@ -376,4 +376,28 @@ int main()
         }
         cout<<endl;
    }
+   // pattern 16
+   /*
+        * * * * *
+        *     *
+        *   *
+        * *
+        * 
+   */
+  // code for pattern 16
+  for (int i=1;i<=5;i++)
+  {
+        for (int j=1;j<=5;j++)
+        {
+            if (j==1 || i==1 || j==6-i)
+            {
+                cout<<"* ";
+            }
+            else
+            {
+                cout<<"  ";
+            }
+        }
+        cout<<endl;
+  }
 }
