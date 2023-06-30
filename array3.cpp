a65bd616e6dfd439ada7ee1adf513dcf893cb2af
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-06-30 23:01:06 +0530

    cin.getline function

--- array3.cpp
+++ array3.cpp
@@ -13,16 +13,16 @@ int length(char input[])
 }
 int main()
 {
-    strings - 1D character array
-    int - 4bites per
-    char - 1byte per
+    //strings - 1D character array
+    //int - 4bites per
+    //char - 1byte per
 
     char name[10];
     cout << "enter your name: ";
     cin >> name;
     // input is coding then from 0-5 coding is print and remaining space have null character "/0" is printed
     cout<<"your name is: "<<name<<endl;
-    cout << length(name);  // length of name
+    cout << length(name)<<endl;;  // length of name
 
     //-----------------------------cin.getline function---------------------
 
@@ -34,9 +34,6 @@ int main()
      cin.getline(input,4);
      cout<<input;
     // output = hel
-
-
-
 
     return 0;
 }
