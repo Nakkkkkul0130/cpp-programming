dd20ee1c7dd149975b5b464bc134ed688aec8f85
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-07-04 22:22:13 +0530

    introduction to 2-D array

---
+++ 2D-array.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[100][100];
+    int m, n;
+    cout<<"enter number of rows: ";
+    cin>>m;
+    cout<<"enter number of column: ";
+    cin>>n;
+
+    for (int i=0;i<m;i++)
+    {
+        for (int j=0;j<n;j++)
+        {
+            cin>>arr[i][j];
+        }
+        cout<<endl;
+    }
+
+    for (int i=0;i<m;i++)
+    {
+        for (int j=0;j<n;j++)
+        {
+            cout<<"value at "<<i <<"th and "<<j<< "th position is: "<<arr[i][j]<<endl;
+        }
+        cout<<endl;
+    }
+
+
+}
