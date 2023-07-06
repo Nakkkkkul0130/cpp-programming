330f61cdf65d181a35e6dbbc97ac7caec1ffdd41
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-07-06 18:03:37 +0530

    initialise 2D-array and pass it to the function

---
+++ 2D-array1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void printarray(int arr[][3],int m,int n)
+{
+    for (int i=0;i<m;i++)
+    {
+        for (int j=0;j<n;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+int main()
+{
+    int arr[][3] = { {2,4,6} , {6,8,10} , {3,5,7} };
+    printarray(arr,3,3);
+    return 0;
+}
