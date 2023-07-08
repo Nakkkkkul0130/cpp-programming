3f6d1e8be24bd5bc206cacaeb2e9b71c518dd710
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-07-08 17:27:32 +0530

    strings

--- strings.cpp
+++ strings.cpp
@@ -17,22 +17,30 @@ int main()
     cout<<*sp<<endl;            // it will print value
 
    // if we have to store multiple string we have to use vector string
-    vector<string> v;
-    v.push_back(s);              // for pushing one element 
-    v.push_back("hello");
-    for (int i=0;i<=v.size();i++)    // for printing the whole content
-    {
-        cout<<v[i]<<endl;
-        sort(v[i].begin(),v[i].end());
-        cout<<v[i]<<endl;
-    }
+        vector<string> v;
+        v.push_back(s);              // for pushing one element 
+        v.push_back("hello");
+        for (int i=0;i<=v.size();i++)    // for printing the whole content
+        {
+            cout<<v[i]<<endl;
+            sort(v[i].begin(),v[i].end());
+            cout<<v[i]<<endl;
+        }
 
     string s4;
-    getline(cin, s4);   // taking input in string
+    //getline(cin, s4);   // taking input in string
     cout<<s4<<endl;
 
     string s5 = s + s2;      // addition of two strings
     cout<<s5<<endl;
     cout<<s5.size()<<endl;   // size of s5
+    cout<<s5.substr(3)<<endl;
+    cout<<s5.find("abc")<<endl;   // this will return int value basically at which index abc is present
+
+    // convert integer into string
+    int a = 123;
+    string s6 = to_string(a);
+    s6.push_back('4');          // insert string in a element
+    cout<<s6<<endl;
     return 0;
 }
