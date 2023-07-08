4dfd55a26bac029b0b078527f11532e293d64bb4
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-07-08 16:51:15 +0530

    strings

---
+++ strings.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main()
+{
+    string s = "abc";   // statically
+    cout<<s<<endl;
+
+    string s2;          // another method for statically 
+    s2 = "def";
+    cout<<s2<<endl;
+
+    string *sp = new string;    // dynamically 
+    *sp = "nakul";
+    cout<<sp<<endl;             // it will print address
+    cout<<*sp<<endl;            // it will print value
+
+   // if we have to store multiple string we have to use vector string
+    vector<string> v;
+    v.push_back(s);              // for pushing one element 
+    v.push_back("hello");
+    for (int i=0;i<=v.size();i++)    // for printing the whole content
+    {
+        cout<<v[i]<<endl;
+        sort(v[i].begin(),v[i].end());
+        cout<<v[i]<<endl;
+    }
+
+    string s4;
+    getline(cin, s4);     // taking input
+    cout<<s4<<endl;
+    return 0;
+}
