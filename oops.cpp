ebe89243d5ef152734e3cb2c06302e9491ddbe55
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-07-09 23:38:00 +0530

    classes and object

--- oops.cpp
+++ oops.cpp
@@ -4,8 +4,29 @@ using namespace std;
 class student {
 
     public:           // used public here because we can access all the properties of a class anywhere rather than in class only
-    int roll_no;
-    int age;
+        int roll_no;
+
+    private:
+        int age;
+
+    public:
+        void display()
+        {
+            cout<<age;
+        }
+        int getage()                //getage function to read
+        {
+            return age;
+        }
+        void setage(int a)             //setage function to write
+        {
+            if(a<0)
+            {
+                return;
+            }
+            age = a;
+        }
+    
 };
 
 int main()
@@ -13,9 +34,11 @@ int main()
     // creating object 
     student s1, s2, s3, s4;
     s1.roll_no = 07;
-    s1.age = 19;
+    // s1.age = 19;
 
-    cout<<s1.age<<endl;
+    //cout<<s1.age<<endl;
     cout<<s1.roll_no<<endl;
-
+    s2.setage(20);
+    s2.display();
+    //cout<<s2.getage()<<endl;
 }
