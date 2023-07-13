5adf671b58006ed9b0059022013331d3e9f702d4
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-07-13 22:47:25 +0530

    object,claases,constructor

--- oops.cpp
+++ oops.cpp
@@ -1,74 +1,69 @@
 #include <iostream>
 using namespace std;
 
-class student {
+class student
+{
 
-        int age;
+    int age;
 
-    public:         // used public here because we can access all the properties of a class anywhere rather than in class only
-
+public: // used public here because we can access all the properties of a class anywhere rather than in class only
         // default constructor
-        
-           student ()
-           {
-                cout<<"constructor called: "<<endl;
-           }
+    student()
+    {
+        cout << "constructor 1 called: " << endl;
+    }
 
-        // parameterised constructor
-            student (int r)
-            {
-                cout<<"constructor 2 called: "<<endl;
-                roll_no = r;
-            }
+    // parameterised constructor
+    student(int r)
+    {
+        cout << "constructor 2 called: " << endl;
+        roll_no = r;
+    }
 
-            student (int a, int r)
-            {
-                cout<<"constructor 3 called: "<<endl;
-                age = a;
-                roll_no = r;
-            }
-        
-    
-        int roll_no;
-        void display()
+    student(int a, int r)
+    {
+        cout << "constructor 3 called: " << endl;
+        age = a;
+        roll_no = r;
+    }
+
+    int roll_no;
+    void display()
+    {
+        cout << age << " " << roll_no << endl;
+    }
+    int getage() // getage function to read
+    {
+        return age;
+    }
+    void setage(int a) // setage function to write
+    {
+        if (a < 0)
         {
-            cout<<age<<endl;
+            return;
         }
-        int getage()                //getage function to read
-        {
-            return age;
-        }
-        void setage(int a)             //setage function to write
-        {
-            if(a<0)
-            {
-                return;
-            }
-            age = a;
-        }
-    
+        age = a;
+    }
 };
 
 int main()
 {
-    // creating object 
+    // creating object
     student s1;
-    s1.display();            // constructor called
-    s1.roll_no = 07;
+    s1.display(); // constructor 1 called
+    // s1.roll_no = 07;
     // s1.age = 19;
 
-    //cout<<s1.age<<endl;
-    cout<<s1.roll_no<<endl;
+    // cout<<s1.age<<endl;
+    // cout << s1.roll_no << endl;
     student s2;
-    s2.setage(20);
-    s2.display();              // constructor called
-    //cout<<s2.getage()<<endl;
+    // s2.setage(20);
+    s2.display();               // constructor 2 called
+    // cout<<s2.getage()<<endl;
 
     student s3(10);
-    s3.display();              // constructor 2 called
+    s3.display(); // constructor 2 called
 
-    student s4(50,100);
-    s4.display();              // constructor 3 called
-
-
+    student s4(50, 100);
+    s4.display(); // constructor 3 called
 }
