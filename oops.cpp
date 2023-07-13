e535bc32544696ad0c116657cf9befe7b3184586
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-07-13 22:32:04 +0530

    object,classes,constructor

--- oops.cpp
+++ oops.cpp
@@ -13,12 +13,26 @@ class student {
            {
                 cout<<"constructor called: "<<endl;
            }
+
+        // parameterised constructor
+            student (int r)
+            {
+                cout<<"constructor 2 called: "<<endl;
+                roll_no = r;
+            }
+
+            student (int a, int r)
+            {
+                cout<<"constructor 3 called: "<<endl;
+                age = a;
+                roll_no = r;
+            }
         
     
         int roll_no;
         void display()
         {
-            cout<<age;
+            cout<<age<<endl;
         }
         int getage()                //getage function to read
         {
@@ -38,14 +52,23 @@ class student {
 int main()
 {
     // creating object 
-    student s1, s2, s3, s4;
+    student s1;
     s1.display();            // constructor called
-    s1.roll_no = 08;
+    s1.roll_no = 07;
     // s1.age = 19;
 
     //cout<<s1.age<<endl;
     cout<<s1.roll_no<<endl;
+    student s2;
     s2.setage(20);
     s2.display();              // constructor called
     //cout<<s2.getage()<<endl;
+
+    student s3(10);
+    s3.display();              // constructor 2 called
+
+    student s4(50,100);
+    s4.display();              // constructor 3 called
+
+
 }
