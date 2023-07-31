0298318f2aa72fc92d701d41f826e085fbe6e8a5
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-07-31 19:59:08 +0530

    oops advance level

---
+++ oops3.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class student 
+{
+    int age;
+    char *name;
+
+    public:
+        student(int age, char *name){
+            this->age = age;
+            this->name = name;
+        }
+        void display()
+        {
+            cout<<name<<" "<<age<<endl;
+        }
+};
+int main()
+{
+    char name[100] = "abcd";
+    student s1(23, name);
+    s1.display();
+
+    student s2(21, name);
+    s2.display();
+
+    return 0;
+}
+
