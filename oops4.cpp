c7f11516016819a13709d324a98b5d4e4911b560
nakul-bhar0130 <115773691+nakul-bhar0130@users.noreply.github.com>
2023-08-01 23:25:27 +0530

    oops advance level

--- oops4.cpp
+++ oops4.cpp
@@ -5,18 +5,27 @@ class student
 {
     public:
         int age;
-        const int rollnumber;
-        int &x;
+        int rollnumber;
+        static int totalstudent;        // static member
+        //int &x;
 
-        student(int r, int a) : rollnumber(r) , age(a), x{this->age}
-        {
 
-        }
+        //student(int r, int a) : rollnumber(r) , age(a), x{this->age}
+        //{
+
+        //}
 };
+
+int student::totalstudent = 0;    //initialise static member 
 
 int main()
 {
+    /*
     student s1(100, 10);
     s1.age = 19;
+    */
+    student s1;
+    cout<<s1.rollnumber<<" "<<s1.age<<endl;
+    cout<<student::totalstudent;         // syntax to use static member
     // s1.rollnumber = 27;
 }
